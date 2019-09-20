54f5f0c600baf4db8c026001a7ff2a9a22268aaf
Alex Bannow <rbannal@gmail.com>
2019-09-20 16:35:07 -0400

    added template for questions and operating start button

--- index.js
+++ index.js
@@ -1,7 +1,43 @@
 'use strict';
 
+const STORE = [
+  {id: cuid(), question: 'first', answers:[ 'an', 'ans', 'answer3', 'answer4'], correctAnswer: 'correct answer'},
+  {id: cuid(), question: 'second', answers:[ 'answer1', 'answer2', 'answer3', 'answer4'], correctAnswer: 'correct answer'},
+  {id: cuid(), question: 'question text', answers:[ 'answer1', 'answer2', 'answer3', 'answer4'], correctAnswer: 'correct answer'},
+  {id: cuid(), question: 'question text', answers:[ 'answer1', 'answer2', 'answer3', 'answer4'], correctAnswer: 'correct answer'},
+  {id: cuid(), question: 'question text', answers:[ 'answer1', 'answer2', 'answer3', 'answer4'], correctAnswer: 'correct answer'},
+  {id: cuid(), questionNumber: 0, score: 0}
+];
+
+function questionNumberStepper() {
+  let currentNum = STORE[5].questionNumber;
+  return currentNum++;
+}
+
+function newFunction(item){
+  let stepper = questionNumberStepper();
+  return `<form class="questions">
+  <fieldset> 
+    <legend class="questionText">${item.question}</legend> 
+  <input type="radio" name="answer 1"> ${item.answers[0]}<br>
+  <input type="radio" name="answer 2"> ${item.answers[1]}<br>
+  <input type="radio" name="answer 3"> ${item.answers[2]}<br>
+  <input type="radio" name="answer 4"> ${item.answers[3]}<br>
+  <input type="submit" value="Submit">
+</fieldset>
+</form>`;
+}
+
+function loadQuestion(question){
+  $('.startQuiz').html(newFunction(question));
+}
+
 function startQuiz() {
-    //when a user clicks on the start quiz button
+    $('.startQuiz').on('click', '.js-button', function(event){
+      
+      loadQuestion(STORE[questionNumberStepper()]);
+    });   
+    
 }
 
 function createQuestion(item) {
@@ -19,6 +55,7 @@ function handleAnswer() {
  
 function restartQuiz() {
   //renders original quiz view
+  //startQuiz();
 }
 
 function doQuiz() {
